6e43afb84103cebc212368275c04febc2cf1fcc3
uraoz <uraot2@gmail.com>
2019-12-03 18:36:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,7 @@ function Phone(){
 }
 function initPage(){
 if ((navigator.userAgent.indexOf("iPhone") > 0 && navigator.userAgent.indexOf("iPad") == -1) || navigator.userAgent.indexOf("iPod") > 0 || navigator.userAgent.indexOf("Android") > 0){
-  var value = document.getElementById("renderer").innerHTML;
-  if(value > 200){
-    Phone();
-  }
+  Phone();
 }else{
 navigator.getUserMedia = ( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.mediaDevices.getUserMedia);
 navigator.getUserMedia({audio:true}, function(stream){
